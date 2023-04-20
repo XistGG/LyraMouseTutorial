b0e440b4878c55798457128d64802facc86a8681
Xist <xist@xist.gg>
2023-04-19 19:30:51 -0700

    Initialize LyraMouseTutorial plugin

---
+++ Source/LyraMouseTutorial/LyraMouseTutorial.Build.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2023 xist.gg
+
+using UnrealBuildTool;
+
+public class LyraMouseTutorial : ModuleRules
+{
+	public LyraMouseTutorial(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+				"CommonInput",
+				"CommonUI",
+				"LyraGame",
+				"UMG",
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ Source/LyraMouseTutorial/Public/LyraMouseTutorial.h
@@ -0,0 +1,15 @@
+﻿// Copyright 2023 xist.gg
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FLyraMouseTutorialModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
