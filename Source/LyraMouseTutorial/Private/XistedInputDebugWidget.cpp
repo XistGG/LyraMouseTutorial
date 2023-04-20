f074c3fa89063248c981ea171c6620f5216a08f1
Xist <xist@xist.gg>
2023-04-20 07:48:46 -0700

    Add XistedInputDebugWidget

---
+++ Source/LyraMouseTutorial/Private/XistedInputDebugWidget.cpp
@@ -0,0 +1,109 @@
+﻿// Copyright 2023 xist.gg
+
+#include "XistedInputDebugWidget.h"
+
+#include "Input/CommonUIActionRouterBase.h"
+
+
+UXistedInputDebugWidget::UXistedInputDebugWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bAutoActivate = true;
+}
+
+
+void UXistedInputDebugWidget::NativeOnActivated()
+{
+	Super::NativeOnActivated();
+
+	// Register for OnActiveInputModeChanged events
+	if (ULocalPlayer* LocalPlayer = GetOwningLocalPlayer())
+	{
+		if (UCommonUIActionRouterBase* ActionRouter = LocalPlayer->GetSubsystem<UCommonUIActionRouterBase>())
+		{
+			ActionRouter->OnActiveInputModeChanged().AddUObject(this, &ThisClass::NativeOnActiveInputModeChanged);
+		}
+	}
+
+	// Simulate that input mode has changed when this widget is activated
+	K2_OnActiveInputModeChanged();
+}
+
+
+void UXistedInputDebugWidget::NativeOnDeactivated()
+{
+	// Unregister for OnActiveInputModeChanged events
+	if (ULocalPlayer* LocalPlayer = GetOwningLocalPlayer())
+	{
+		if (UCommonUIActionRouterBase* ActionRouter = LocalPlayer->GetSubsystem<UCommonUIActionRouterBase>())
+		{
+			ActionRouter->OnActiveInputModeChanged().RemoveAll(this);
+		}
+	}
+
+	Super::NativeOnDeactivated();
+}
+
+
+void UXistedInputDebugWidget::GetUIInputConfigData(FText& MouseCaptureModeText, FText& MouseLockModeText, bool& bIsCursorVisible, bool& bIsLookInputIgnored, bool& bIsMoveInputIgnored)
+{
+	// Initialize outputs as empty
+
+	MouseCaptureModeText = FText::FromString(TEXT("n/a"));
+	MouseLockModeText = FText::FromString(TEXT("n/a"));
+
+	bIsCursorVisible = bIsLookInputIgnored = bIsMoveInputIgnored = false;
+
+	// Populate outputs if possible
+
+	if (ULocalPlayer* LocalPlayer = GetOwningLocalPlayer())
+	{
+		APlayerController* PC = LocalPlayer->GetPlayerController(GetWorld());
+		UGameViewportClient* GameViewportClient = LocalPlayer->ViewportClient;
+
+		if (PC)
+		{
+			bIsCursorVisible = PC->ShouldShowMouseCursor();
+			bIsLookInputIgnored = PC->IsLookInputIgnored();
+			bIsMoveInputIgnored = PC->IsMoveInputIgnored();
+		}
+
+		if (GameViewportClient)
+		{
+			switch (const EMouseCaptureMode MouseCaptureMode = GameViewportClient->GetMouseCaptureMode())
+			{
+			case EMouseCaptureMode::CapturePermanently:
+				MouseCaptureModeText = FText::FromString(TEXT("CapturePermanently"));
+				break;
+			case EMouseCaptureMode::CapturePermanently_IncludingInitialMouseDown:
+				MouseCaptureModeText = FText::FromString(TEXT("CapturePermanently_IncludingInitialMouseDown"));
+				break;
+			case EMouseCaptureMode::CaptureDuringMouseDown:
+				MouseCaptureModeText = FText::FromString(TEXT("CaptureDuringMouseDown"));
+				break;
+			case EMouseCaptureMode::CaptureDuringRightMouseDown:
+				MouseCaptureModeText = FText::FromString(TEXT("CaptureDuringRightMouseDown"));
+				break;
+			case EMouseCaptureMode::NoCapture:
+				MouseCaptureModeText = FText::FromString(TEXT("NoCapture"));
+				break;
+			default:
+				MouseCaptureModeText = FText::FromString(TEXT("UNKNOWN"));
+				break;
+			}
+
+			if (GameViewportClient->ShouldAlwaysLockMouse())
+			{
+				MouseLockModeText = FText::FromString(TEXT("Always Lock"));
+			}
+			else if (GameViewportClient->LockDuringCapture())
+			{
+				MouseLockModeText = FText::FromString(TEXT("Lock During Capture"));
+			}
+			else
+			{
+				MouseLockModeText = FText::FromString(TEXT("Not Locked"));
+			}
+		}
+	}
+}
---
+++ Source/LyraMouseTutorial/Public/XistedInputDebugWidget.h
@@ -0,0 +1,52 @@
+﻿// Copyright 2023 xist.gg
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/LyraActivatableWidget.h"
+#include "XistedInputDebugWidget.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FXistedOnActiveInputModeChanged);
+
+
+/**
+ * XistEd Input Debug Widget
+ *
+ * I created a widget in my Lyra's `Content/UI/W_XistedInputDebug`
+ * based on this class.  Then I hooked it up to the ShooterCore's `B_ShooterGame_Elimination`
+ * experience, registered to the `HUD.Slot.InfrequentAbilities` widget extension point.
+ *
+ * This is *NOT REQUIRED*, it is purely a hack whose intent is to make it easier for you to
+ * see what the current input mode is and when it is being changed.
+ */
+UCLASS()
+class LYRAMOUSETUTORIAL_API UXistedInputDebugWidget : public ULyraActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	// Set Class Defaults
+	UXistedInputDebugWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void NativeOnActivated() override;
+	virtual void NativeOnDeactivated() override;
+
+	/**
+	 * The actual values for mouse capture state and mouse lock state are NOT available to BP,
+	 * and rather than hack the Engine and Lyra to make them available, I'm just exposing
+	 * text descriptions of their values here.
+	 */
+	UFUNCTION(BlueprintCallable, DisplayName="Get UI Input Config Data")
+	void GetUIInputConfigData(FText& MouseCaptureModeText, FText& MouseLockModeText, bool& bIsCursorVisible, bool& bIsLookInputIgnored, bool& bIsMoveInputIgnored);
+
+	/** Implement this in your Widget BP and update your widget values every time this event fires */
+	UFUNCTION(BlueprintImplementableEvent, DisplayName="On Active Input Mode Changed")
+	void K2_OnActiveInputModeChanged();
+
+	/** Called by the CommonUIActionRouterBase subsystem only when this widget is active */
+	UFUNCTION()
+	void NativeOnActiveInputModeChanged(ECommonInputMode NewInputMode) { K2_OnActiveInputModeChanged(); }
+
+};
