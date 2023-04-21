a2979b1d7d8117f688238e8767e95f8ff840981a
Xist <xist@xist.gg>
2023-04-21 09:41:24 -0700

    Add XistedSetInputMode static BP callable method

---
+++ Source/LyraMouseTutorial/Private/XistedInputModeStatics.cpp
@@ -0,0 +1,54 @@
+﻿// Copyright 2023 xist.gg
+// @see https://github.com/XistGG/LyraMouseTutorial
+
+#include "XistedInputModeStatics.h"
+
+#include "XistedLogMacros.h"
+#include "XistedUIActionRouter.h"
+
+
+void UXistedInputModeStatics::XistedSetInputMode(APlayerController* PlayerController, bool bMouseVisible, bool bIgnoreLookInput, bool bIgnoreMoveInput)
+{
+	// Make sure the controller is connected to a local player
+	const ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+	if (!LocalPlayer)
+	{
+		XISTED_ERROR_LOG(TEXT("LocalPlayer is null"));
+		return;
+	}
+
+	// We only need the base action router, not our custom one, though none of this
+	// will work the way we want if this hasn't been overridden to use our custom
+	// action router subsystem.
+	UCommonUIActionRouterBase* ActionRouter = LocalPlayer->GetSubsystem<UCommonUIActionRouterBase>();
+	if (!ActionRouter)
+	{
+		XISTED_ERROR_LOG(TEXT("Common UI Action Router subsystem is not available"));
+		return;
+	}
+
+	// Create the new desired UI Input Config
+	FUIInputConfig NewInputConfig;
+	if (bMouseVisible)
+	{
+		// Input settings when mouse is Visible
+		constexpr bool bHideCursorDuringViewportCapture = false;
+		NewInputConfig = FUIInputConfig(ECommonInputMode::All, EMouseCaptureMode::NoCapture, bHideCursorDuringViewportCapture);
+	}
+	else
+	{
+		// Input settings when mouse is Invisible
+		constexpr bool bHideCursorDuringViewportCapture = true;
+		NewInputConfig = FUIInputConfig(ECommonInputMode::Game, EMouseCaptureMode::CapturePermanently_IncludingInitialMouseDown, bHideCursorDuringViewportCapture);
+	}
+
+	// Apply Look/Move ignore switches
+	NewInputConfig.bIgnoreLookInput = bIgnoreLookInput;
+	NewInputConfig.bIgnoreMoveInput = bIgnoreMoveInput;
+
+	// Set mouse visibility just *before* we change the UI Input Config
+	PlayerController->SetShowMouseCursor(bMouseVisible);
+
+	// Tell the Action Router to use this new input config
+	ActionRouter->SetActiveUIInputConfig(NewInputConfig);
+}
---
+++ Source/LyraMouseTutorial/Public/XistedInputModeStatics.h
@@ -0,0 +1,33 @@
+﻿// Copyright 2023 xist.gg
+// @see https://github.com/XistGG/LyraMouseTutorial
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "XistedInputModeStatics.generated.h"
+
+
+/**
+ * XistEd Input Mode Statics
+ *
+ * You don't have to use a blueprint function library to do this, I'm just doing it this way
+ * for example purposes.  The key is in the implementation of the XistedSetInputMode method,
+ * which you can implement anywhere in your code that you want.
+ */
+UCLASS()
+class LYRAMOUSETUTORIAL_API UXistedInputModeStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Set a new Input Mode for this player
+	 * @param PlayerController Player Controller whose input mode to set
+	 * @param bMouseVisible Whether the mouse should be visible (true) or invisible (false)
+	 * @param bIgnoreLookInput Whether look input should now be ignored
+	 * @param bIgnoreMoveInput Whether move input should now be ignored
+	 */
+	UFUNCTION(BlueprintCallable)
+	static void XistedSetInputMode(APlayerController* PlayerController, bool bMouseVisible, bool bIgnoreLookInput, bool bIgnoreMoveInput);
+
+};
